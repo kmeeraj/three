e20c66821d512172b05da6920e20d3d1c16d1e9d
meeraj kanaparthi <meeraj.kanaparthi@teradata.com>
2019-02-10 21:32:53 +0530

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,77 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, Component, ElementRef, ViewChild} from '@angular/core';
+import {Camera, Color, PerspectiveCamera, Renderer, Scene, WebGLRenderer} from 'three';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'threed';
+  scene: Scene;
+  camera: Camera;
+  renderer: Renderer;
+  public fieldOfView = 60;
+  public nearClippingPane = 1;
+  public farClippingPane = 1000;
+
+  constructor() {
+    this.render = this.render.bind(this);
+  }
+
+  @ViewChild('canvas')
+  private canvasRef: ElementRef;
+
+  private get canvas(): HTMLCanvasElement {
+    return this.canvasRef.nativeElement;
+  }
+
+
+  ngAfterViewInit() {
+    this.createScene();
+    this.createCamera();
+    this.startRendering();
+  }
+
+  private createScene() {
+    this.scene = new Scene();
+    this.scene.background = new Color(0x123456);
+  }
+
+  private createCamera() {
+    let aspectRatio = this.getAspectRatio();
+    this.camera = new PerspectiveCamera(this.fieldOfView,
+      aspectRatio,
+      this.nearClippingPane,
+      this.farClippingPane
+    );
+    this.camera.position.z = 5;
+  }
+
+  private getAspectRatio(): number {
+    let height = this.canvas.clientHeight;
+    if (height === 0) {
+      return 0;
+    }
+    return this.canvas.clientWidth / this.canvas.clientHeight;
+  }
+
+  private startRendering() {
+    this.renderer = new WebGLRenderer({
+      canvas: this.canvas,
+      antialias: true
+    });
+    this.renderer.setSize(this.canvas.clientWidth, this.canvas.clientHeight);
+    let component: AppComponent = this;
+
+    (function render() {
+      requestAnimationFrame(render);
+      component.render();
+    }());
+  }
+
+  public render() {
+    console.log("Hello");
+    this.renderer.render(this.scene, this.camera);
+  }
 }
