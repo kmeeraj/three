c004fb34b9272bfe4917c1a92d811cb6248e4335
meeraj kanaparthi <meeraj.kanaparthi@teradata.com>
2019-02-11 19:25:52 +0530

    material

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,18 @@
 import {AfterViewInit, Component, ElementRef, ViewChild} from '@angular/core';
-import {Camera, Color, PerspectiveCamera, Renderer, Scene, WebGLRenderer} from 'three';
+import {
+  AxesHelper,
+  BoxGeometry,
+  Camera,
+  Color,
+  Mesh,
+  MeshBasicMaterial,
+  PerspectiveCamera,
+  PlaneGeometry,
+  Renderer,
+  Scene,
+  WebGLRenderer
+} from 'three';
+import {getOrSetAsInMap} from '@angular/animations/browser/src/render/shared';
 
 @Component({
   selector: 'app-root',
@@ -11,9 +24,15 @@ export class AppComponent implements AfterViewInit {
   scene: Scene;
   camera: Camera;
   renderer: Renderer;
+  cube1: Mesh;
+  cube2: Mesh;
+  plane: Mesh;
+  axes: AxesHelper;
+
   public fieldOfView = 60;
   public nearClippingPane = 1;
   public farClippingPane = 1000;
+  private ADD = 0.1;
 
   constructor() {
     this.render = this.render.bind(this);
@@ -30,6 +49,8 @@ export class AppComponent implements AfterViewInit {
   ngAfterViewInit() {
     this.createScene();
     this.createCamera();
+    this.createGeometry();
+    this.createAxes();
     this.startRendering();
   }
 
@@ -45,7 +66,9 @@ export class AppComponent implements AfterViewInit {
       this.nearClippingPane,
       this.farClippingPane
     );
-    this.camera.position.z = 5;
+    this.camera.position.z = 30;
+    this.camera.position.y = 3;
+    this.camera.position.x = 4;
   }
 
   private getAspectRatio(): number {
@@ -71,7 +94,45 @@ export class AppComponent implements AfterViewInit {
   }
 
   public render() {
-    console.log("Hello");
+    this.cube1.position.x += this.ADD;
+    this.cube2.position.x -= this.ADD;
+    if(this.cube1.position.x > 6 || this.cube1.position.x < -6){
+      this.ADD *= -1;
+    }
     this.renderer.render(this.scene, this.camera);
+  }
+
+  private createGeometry() {
+    let geometry = new BoxGeometry(5,5, 5);
+    let material = new MeshBasicMaterial({color: 0Xc9b92b});
+
+    this.cube1 = new Mesh(geometry, material);
+    this.cube1.position.z = -6;
+    this.cube1.position.y = -5;
+
+    geometry = new BoxGeometry(5, 5, 5);
+    material = new MeshBasicMaterial({color: 0Xff0040, transparent:true, opacity: 0.5});
+
+    this.cube2 = new Mesh(geometry, material);
+    this.cube2.position.z = 6;
+    this.cube2.position.y = -5;
+
+    let planegeometry = new PlaneGeometry(1000, 1000, 50, 50);
+    material = new MeshBasicMaterial({color: 0Xa6f995, wireframe: true});
+
+    this.plane = new Mesh(planegeometry, material);
+    this.plane.rotation.x = Math.PI / 2;
+    this.plane.position.y = -100;
+
+    this.scene.add(this.cube1);
+    this.scene.add(this.cube2);
+    this.scene.add(this.plane);
+
+
+  }
+
+  private createAxes() {
+    this.axes = new AxesHelper(15);
+    this.scene.add(this.axes);
   }
 }
