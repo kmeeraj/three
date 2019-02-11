c3d958e0fbdde65913c134c52b4dd447df2b3853
meeraj kanaparthi <meeraj.kanaparthi@teradata.com>
2019-02-11 18:32:45 +0530

    funny

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -39,8 +39,43 @@ export class AppComponent implements AfterViewInit {
   createFont() {
     this.loader = new FontLoader();
     let font = this.loader.parse(star_war_font);
-    let titles = 'The Lord shall bless the out of Zion, \n Jesus Christ, \n Halleujah, \n Praise the Lord, \n Thank you Jesus, \n Maranatha';
-    this.geometry = new TextGeometry(titles, {font: font, size: 3, height: 0.1});
+    // let titles = 'The Lord shall bless the out of Zion, \n Jesus Christ, \n Halleujah, \n Praise the Lord, \n Thank you Jesus, \n Maranatha';
+    let titles = 'Star wars : SV1.0 \n' +
+      '\n' +
+      'After the Star Wars : Last Jedi, \n' +
+      'General Leia lost her hopes on Jedi, \n' +
+      'because  of Luke Skywaker’s stubbornness.\n' +
+      'Even thou General Leia is happy about Rey \n' +
+      'in front of her, \n' +
+      'she is totally jealous of her killer looks, \n' +
+      'she is afraid all upcoming newly recruited \n' +
+      'apprentice trained under Rey,\n' +
+      ' will not listen to General Leia. \n' +
+      'So after loosing all her kick ass weapons \n' +
+      'like super laser beams \n' +
+      'and flying aircraft carriers \n' +
+      '(which was later stolen by avengers), General Leia \n' +
+      'looks for left over options. \n' +
+      'Following are the left over options:\n' +
+      '    1. Facebook\n' +
+      '    2. Instagram\n' +
+      '    3. Twitter\n' +
+      '    4. Google\n' +
+      '    5. Chewbacca\n' +
+      '    6. R2 - D2 \n'+
+      '    7. Spare parts for broken aircraft\n'+
+      '    6. And lots of lots of data scientists, \n' +
+      'from United States of Ameerpet\n' +
+      'Now she don’t want to data scientists \n' +
+      'to be trained as Jedi \n' +
+      '(to stop influence of Ray, because of \n' +
+      'her killer looks and \n' +
+      'she hardly find facebook followers and friends), \n' +
+      'she find the the only hope for Resitance to \n' +
+      'fight against the federation that is SV1.0 \n' +
+      'aka Social Vantange 1.0. \n' +
+      'And one day... story continues..\n';
+      this.geometry = new TextGeometry(titles, {font: font, size: 3, height: 0.1});
     let material = new MeshBasicMaterial({color: 0xffffff});
     this.text = new Mesh(this.geometry, material);
     this.text.position.x = -25;
