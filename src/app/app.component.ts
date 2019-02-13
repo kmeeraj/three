92faa1f09a2379cd9151e80261f5cecefa96c7e8
meeraj kanaparthi <meeraj.kanaparthi@teradata.com>
2019-02-14 00:43:54 +0530

    mouse click

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,17 @@
 import {AfterViewInit, Component, ElementRef, ViewChild} from '@angular/core';
-import {Camera, Color, PerspectiveCamera, Renderer, Scene, WebGLRenderer} from 'three';
+import {
+  BoxGeometry,
+  Camera,
+  Color, DirectionalLight,
+  DoubleSide, Mesh,
+  MeshPhongMaterial,
+  PerspectiveCamera,
+  Raycaster,
+  Renderer,
+  Scene,
+  SphereGeometry, Vector2, Vector3,
+  WebGLRenderer
+} from 'three';
 
 @Component({
   selector: 'app-root',
@@ -14,6 +26,14 @@ export class AppComponent implements AfterViewInit {
   public fieldOfView = 60;
   public nearClippingPane = 1;
   public farClippingPane = 1000;
+  private sphere: Mesh;
+  private cube: Mesh;
+  private light1: DirectionalLight;
+  private light2: DirectionalLight;
+  private rayCast: Raycaster;
+  private mouse: Vector3;
+  private spherematerial: MeshPhongMaterial;
+  private boxMaterial: MeshPhongMaterial;
 
   constructor() {
     this.render = this.render.bind(this);
@@ -30,12 +50,15 @@ export class AppComponent implements AfterViewInit {
   ngAfterViewInit() {
     this.createScene();
     this.createCamera();
+    this.createGeometry();
+    this.createLight();
     this.startRendering();
+    this.canvas.addEventListener('click', this.mouseclick.bind(this), false);
   }
 
   private createScene() {
     this.scene = new Scene();
-    this.scene.background = new Color(0x123456);
+    this.scene.background = new Color(0xffffff);
   }
 
   private createCamera() {
@@ -45,7 +68,7 @@ export class AppComponent implements AfterViewInit {
       this.nearClippingPane,
       this.farClippingPane
     );
-    this.camera.position.z = 5;
+    this.camera.position.set(0, 10, 40);
   }
 
   private getAspectRatio(): number {
@@ -71,7 +94,46 @@ export class AppComponent implements AfterViewInit {
   }
 
   public render() {
-    console.log("Hello");
+
+    this.sphere.material['color'].set(0x0450fb);
+    this.cube.material['color'].set(0xff4500);
+    let intersects = this.rayCast.intersectObjects(this.scene.children);
+    console.log(intersects[0]);
+    intersects.forEach((i) => i.object['material']['color'].set(0x00ff00));
     this.renderer.render(this.scene, this.camera);
+  }
+
+  private createGeometry() {
+    let geometry = new SphereGeometry (5, 30 , 30);
+     this.spherematerial = new MeshPhongMaterial({color: 0x0450fb, shininess: 100, side: DoubleSide});
+    this.sphere = new Mesh(geometry, this.spherematerial);
+    this.sphere.position.set(1, 4, -10);
+
+    let geo = new BoxGeometry(5, 5, 5);
+    this.boxMaterial = new MeshPhongMaterial({color: 0xff4500, shininess:100, side: DoubleSide});
+    this.cube = new Mesh(geo, this.boxMaterial);
+    this.cube.position.set(10, 4 , -10);
+    this.scene.add(this.cube);
+    this.scene.add(this.sphere);
+
+  }
+
+  private createLight() {
+    this.light1 = new DirectionalLight(0xffffff,1);
+    this.light2 = new DirectionalLight(0xffffff,1);
+    this.light2.position.set(0, -5, 2);
+    this.scene.add(this.light1);
+    this.scene.add(this.light2);
+
+    this.rayCast = new Raycaster();
+    this.mouse = new Vector3();
+    this.mouse.x = this.mouse.y = -1;
+  }
+
+  private mouseclick(e: MouseEvent) {
+    this.mouse.x = (e.clientX / window.innerWidth) * 2 - 1;
+    this.mouse.y = -(e.clientX / window.innerWidth) * 2 + 1;
+    this.mouse.z = 1;
+    this.rayCast.setFromCamera(this.mouse, this.camera);
   }
 }
